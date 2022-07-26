c87164db2da584abb6f76c68cbf04b4dda67f136
Bulintis Stefan Claudiu <93158574+stefanbulintis@users.noreply.github.com>
2022-07-26 17:07:43 +0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $(".cards").click(function() {
+//             let link = document.createElement('a');
+//             link.href = '#/norris.html';
+//             link.innerText = 'Norris';
+//         })
+//     });
